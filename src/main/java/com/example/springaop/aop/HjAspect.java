9844b47d8ab885d31d2c9a6da57195762eb8fc09
1262741800@qq.com <hujie20122736>
2020-02-04 22:38:47 +0800

    环绕增强实现方法耗时计算

--- src/main/java/com/example/springaop/aop/HjAspect.java
+++ src/main/java/com/example/springaop/aop/HjAspect.java
@@ -1,17 +1,33 @@
 package com.example.springaop.aop;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HjAspect {
+    private Logger logger = LoggerFactory.getLogger(HjAspect.class);
+
     @Pointcut("execution(* com.example.springaop.service.*.*(..))")
     private void pointcut() {
 
+    }
+
+    @Around("com.example.springaop.aop.HjAspect.pointcut()")
+    public Object around(ProceedingJoinPoint pjp) {
+        long start = System.currentTimeMillis();
+        Object proceed = null;
+        try {
+            proceed = pjp.proceed();
+            long end = System.currentTimeMillis();
+            logger.info("方法耗时：" + (end - start));
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return proceed;
     }
 
     @Before("com.example.springaop.aop.HjAspect.pointcut()")
